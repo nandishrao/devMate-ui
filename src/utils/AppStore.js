40b8a5ef83731c626d0557405f81a54dffcf20e6
nandishrao <nandishhraoo@gmail.com>
2025-10-11 14:17:54 +0530

    added connection slice
    Signed-off-by: nandishrao <nandishhraoo@gmail.com>

--- src/utils/AppStore.js
+++ src/utils/AppStore.js
@@ -1,12 +1,13 @@
 import userReducer from "./userSlice"
 import feedReducer from "./feedSlice"
+import connectionReduceer from "./Connectionslice"
 import { configureStore } from "@reduxjs/toolkit"
 
 const appStore = configureStore({
     reducer : {
         user : userReducer,
         feed : feedReducer,
-
+        connections : connectionReduceer,
     }
 })
 export default appStore
