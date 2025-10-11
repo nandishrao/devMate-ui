14a2bac359917430cdd6d4672107a188587dacb6
nandishrao <nandishhraoo@gmail.com>
2025-10-11 12:36:25 +0530

    Connections route
    Signed-off-by: nandishrao <nandishhraoo@gmail.com>

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { Provider } from "react-redux";
 import appStore from "./utils/AppStore";
 import Feed from "./Components/Feed";
 import Profile from "./Components/Profile";
+import Connections from "./Components/Connection";
 function App() {
   return (
   <Provider store={appStore}>
@@ -13,6 +14,8 @@ function App() {
         <Route path="/" element={<Body />}>
               <Route path="/" element={<Feed />} />
               <Route path="/login" element={<Login />} />
+              <Route path="/profile" element={<Profile />} />
+              <Route path="/connections" element={<Connections/>} />
               <Route path="/profile" element={<Profile />} />
         </Route>
       </Routes> 
