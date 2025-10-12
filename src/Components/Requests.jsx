d261bdac487336e96c8a3c260d970d6bd3c96d9e
nandishrao <nandishhraoo@gmail.com>
2025-10-12 09:38:39 +0530

    added connection request accepting feature
    Signed-off-by: nandishrao <nandishhraoo@gmail.com>

--- src/Components/Requests.jsx
+++ src/Components/Requests.jsx
@@ -2,17 +2,27 @@ import { useEffect } from "react";
 import { BASE_URL } from "../Constants/constants";
 import axios from "axios";
 import { useDispatch, useSelector } from "react-redux";
-import { addRequests } from "../utils/requestsSlice";
+import { addRequests, removeRequests } from "../utils/requestsSlice";
 
 const Requests = () => {
   const requests = useSelector((store) => store.requests);
   const dispatch = useDispatch();
+
+  const handelReview = async (status, _id) => {
+    try {
+      const res = await axios.post(
+        BASE_URL + "/request/review/" + status + "/" + _id,
+        {},
+        { withCredentials: true }
+      );
+      dispatch(removeRequests(_id));
+    } catch (err) {}
+  };
   const fetchRequests = async () => {
     const res = await axios.get(BASE_URL + "/user/requests/showInterested", {
       withCredentials: true,
     });
     dispatch(addRequests(res.data.connectionRequest));
-    console.log(res.data.connectionRequest);
   };
   useEffect(() => {
     if (requests) return;
@@ -51,20 +61,21 @@ const Requests = () => {
                 <p>
                   {age} {gender}
                 </p>
-               <div className="flex">
-                 <button
-                  type="button"
-                  class="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
-                >
-                  Ignore
-                </button>
-                <button
-                  type="button"
-                  class="text-white bg-blue-700 hover:bg-blue-800  font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
-                >
-                  Accept
-                </button>
-               </div>
+                <div className="flex">
+                  <button
+                    type="button"
+                    className="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
+                  >
+                    Ignore
+                  </button>
+                  <button
+                    type="button"
+                    className="text-white bg-blue-700 hover:bg-blue-800  font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800 cursor-pointer"
+                    onClick={() => handelReview("accepted", request._id)}
+                  >
+                    Accept
+                  </button>
+                </div>
               </div>
             </div>
           );
--- src/utils/requestsSlice.js
+++ src/utils/requestsSlice.js
@@ -5,8 +5,11 @@ const requestsSlice = createSlice({
   initialState: null,
   reducers: {
     addRequests: (state, action) => action.payload,
-    // removeRequests: (state, action) => action.payload,
+    removeRequests: (state, action) =>{
+       const newArray = state.filter((r) => r._id !== action.payload);
+      return newArray;
+    },
   },
 });
 export const { addRequests, removeRequests } = requestsSlice.actions;
-export default requestsSlice.reducer
+export default requestsSlice.reducer;
